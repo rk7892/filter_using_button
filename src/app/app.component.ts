ede6e665c935d89b4aecd1248c9a7b1386076578
rk7892 <ranjithraina7952@gmail.com>
2024-02-16 15:16:42 +0530

    create filter method using button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,67 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ReactiveFormsModule, CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'filter_using_button';
+
+  listData: any;
+  constructor() {
+    this.listData = [
+      {
+        name: 'ranjith',
+        age: 25,
+        phone: 9524414213,
+        location: 'tvmalai',
+      },
+      {
+        name: 'ranjith',
+        age: 22,
+        phone: 123456789,
+        location: 'chennai',
+      },
+      {
+        name: 'Hari',
+        age: 42,
+        phone: 66666,
+        location: 'pondicheri',
+      },
+      {
+        name: 'Hari',
+        age: 12,
+        phone: 222,
+        location: 'Vellore',
+      },
+      {
+        name: 'Dinesh',
+        age: 16,
+        phone: 222,
+        location: 'Chennai',
+      },
+      {
+        name: 'Dinesh',
+        age: 22,
+        phone: 5555,
+        location: 'tvmalai',
+      },
+    ];
+  }
+
+  ranjith() {
+    this.listData = this.listData.filter((item: any) => item.name === "ranjith")
+  }
+  hari() {
+    this.listData = this.listData.filter((item: any) => item.phone === 222)
+  }
+  dinesh() {
+    this.listData = this.listData.filter((item: any) => item.location === "tvmalai")
+  }
 }
